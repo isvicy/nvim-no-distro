8edae8b35e06ce71ae7c0420b428715880742fef
isvicy <isregistermail@gmail.com>
2025-06-15 22:53:13 +0800

    feat(lsp): use lsp-config as config provider

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -6,6 +6,9 @@ vim.lsp.enable({
 
 return {
   {
+    'neovim/nvim-lspconfig',
+  },
+  {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
