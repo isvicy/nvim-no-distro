58e18363ad9c6f765282d91603e3577d7a203b0c
isvicy <isregistermail@gmail.com>
2025-07-30 22:32:37 +0800

    feat(editor): make more use of flash.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -206,7 +206,7 @@ return {
     end,
     config = function(_, opts)
       require('fzf-lua').setup(opts)
-      require("fzf-lua").register_ui_select()
+      require('fzf-lua').register_ui_select()
     end,
     keys = {
       { '<c-j>', '<c-j>', ft = 'fzf', mode = 't', nowait = true },
@@ -331,9 +331,61 @@ return {
   },
   {
     'folke/flash.nvim',
+    event = 'VeryLazy',
     opts = {
+      modes = {
+        search = {
+          enabled = true,
+        },
+        char = {
+          enabled = true,
+          jump_labels = true,
+        },
+      },
       label = {
         after = { 0, 2 },
+      },
+    },
+    keys = {
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+      },
+      {
+        'r',
+        mode = 'o',
+        function()
+          require('flash').remote()
+        end,
+        desc = 'Remote Flash',
+      },
+      {
+        'R',
+        mode = { 'o', 'x' },
+        function()
+          require('flash').treesitter_search()
+        end,
+        desc = 'Treesitter Search',
+      },
+      {
+        '<c-s>',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
       },
     },
   },
