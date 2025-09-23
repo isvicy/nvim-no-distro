ff918bfd0335cc34899c72ba6326e3c7eddbdb13
isvicy <isregistermail@gmail.com>
2025-09-23 17:27:15 +0800

    feat(plugins): add plugin for screenshot

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,30 +1,26 @@
 return {
   {
-    'neet-007/rfc-view.nvim',
-    branch = 'master',
-    build = 'cd go && go build main.go',
-    config = function()
-      require('rfcview').setup {
-        -- Automatically delete RFC buffers when closing
-        delete_buffers_when_closing = true,
-      }
-    end,
+    'mistricky/codesnap.nvim',
+    build = 'make build_generator',
     keys = {
-      -- Global keymaps to open and close the plugin's main interface
       {
-        '<leader>ro',
-        function()
-          require('rfcview').open_rfc()
-        end,
-        desc = '[R]FC [O]pen Main Window',
+        '<leader>ccc',
+        '<cmd>CodeSnap<cr>',
+        mode = 'x',
+        desc = 'Save selected code snapshot into clipboard',
       },
       {
-        '<leader>rc',
-        function()
-          require('rfcview').close_rfc()
-        end,
-        desc = '[R]FC [C]lose All Windows',
+        '<leader>cs',
+        '<cmd>CodeSnapSave<cr>',
+        mode = 'x',
+        desc = 'Save selected code snapshot in ~/Pictures',
       },
+    },
+    opts = {
+      save_path = '~/Pictures',
+      has_breadcrumbs = true,
+      bg_color = "#535c68",
+      watermark = "",
     },
   },
 }
