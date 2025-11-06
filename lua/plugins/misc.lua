ad635e687c651caf6421a98ebeaa502059ab2327
isvicy <isregistermail@gmail.com>
2025-11-06 22:46:11 +0800

    feat(misc): add aerial

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -19,8 +19,27 @@ return {
     opts = {
       save_path = '~/Pictures',
       has_breadcrumbs = true,
-      bg_color = "#535c68",
-      watermark = "",
+      bg_color = '#535c68',
+      watermark = '',
+    },
+  },
+  {
+    'stevearc/aerial.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    keys = {
+      {
+        '<leader>co',
+        function()
+          require('aerial').fzf_lua_picker()
+        end,
+        mode = 'n',
+        desc = 'searching outline',
+      },
+    },
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
     },
   },
 }
