1a32aaf0f7336f86e23d3d700a0f4f18eacf822b
isvicy <isregistermail@gmail.com>
2025-11-23 13:32:12 +0800

    feat: add config for nil_ls

---
+++ lsp/nil_ls.lua
@@ -0,0 +1,21 @@
+return {
+  cmd = {
+    'nil',
+  },
+  filetypes = {
+    'lua',
+  },
+  root_markers = {
+    'flake.nix',
+    '.git',
+  },
+  settings = {
+    ['nil'] = {
+      nix = {
+        flake = {
+          autoArchive = false,
+        },
+      },
+    },
+  },
+}
