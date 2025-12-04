b1d4b308afde15b3124283063dc12ff47c1bc137
isvicy <isregistermail@gmail.com>
2025-12-04 12:29:39 +0800

    feat: use forked nvterm to use per path terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,36 +1,57 @@
 return {
   {
-    'NvChad/nvterm',
-
+    'isvicy/nvterm',
     keys = {
-
+      -- Per-path terminals (Alt + key)
       {
         '<A-h>',
+        function()
+          require('nvterm.terminal').toggle_per_path('horizontal')
+        end,
+        mode = { 'n', 't', 'i' },
+        desc = 'Toggle Horizontal Terminal (per-path)',
+      },
+      {
+        '<A-v>',
+        function()
+          require('nvterm.terminal').toggle_per_path('vertical')
+        end,
+        mode = { 'n', 't', 'i' },
+        desc = 'Toggle Vertical Terminal (per-path)',
+      },
+      {
+        '<A-f>',
+        function()
+          require('nvterm.terminal').toggle_per_path('float')
+        end,
+        mode = { 'n', 't', 'i' },
+        desc = 'Toggle Floating Terminal (per-path)',
+      },
+
+      -- General terminals (Alt + Ctrl + key)
+      {
+        '<A-C-h>',
         function()
           require('nvterm.terminal').toggle('horizontal')
         end,
         mode = { 'n', 't', 'i' },
-        desc = 'Toggle Horizontal Terminal',
+        desc = 'Toggle Horizontal Terminal (general)',
       },
       {
-        '<A-v>',
-
+        '<A-C-v>',
         function()
           require('nvterm.terminal').toggle('vertical')
         end,
-
         mode = { 'n', 't', 'i' },
-        desc = 'Toggle Vertical Terminal',
+        desc = 'Toggle Vertical Terminal (general)',
       },
-
       {
-        '<A-f>',
+        '<A-C-f>',
         function()
           require('nvterm.terminal').toggle('float')
         end,
-
         mode = { 'n', 't', 'i' },
-        desc = 'Toggle Floating Terminal',
+        desc = 'Toggle Floating Terminal (general)',
       },
     },
     opts = {},
